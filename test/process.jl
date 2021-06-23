d0f93d347baf027ce565dcc608a522ea79aa1b32
Colin Swaney <colinswaney@princeton.edu>
2021-06-22 20:21:20 -0400

    Update process.jl

--- test/process.jl
+++ test/process.jl
@@ -4,10 +4,10 @@ Pkg.activate(".")
 using TotalViewITCH
 using Dates
 
-file = "/Users/colinswaney/Data/itch/bin/S031413-v41.txt"
+file = "./data/bin/S031413-v41.txt"
 version = 4.1
 date = Date("2013-03-14")
 nlevels = 3
 tickers = ["ALT"]
-dir = "/Users/colinswaney/Desktop/Development/TotalViewITCH/data"
+dir = "./data/csv"
 process(file, version, date, nlevels, tickers, dir)
