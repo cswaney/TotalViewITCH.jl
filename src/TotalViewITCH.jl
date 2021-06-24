953c44bc612d2f8239949579438f455f6a5f1659
Colin Swaney <colinswaney@princeton.edu>
2021-06-23 22:07:10 -0400

    Update TotalViewITCH.jl
    - Export Message, Order and Book types

--- src/TotalViewITCH.jl
+++ src/TotalViewITCH.jl
@@ -3,15 +3,9 @@ module TotalViewITCH
 using Dates, ProgressMeter
 
 include("messages.jl")
-export AbstractMessage, Message, NOIIMessage, TradeMessage, split, complete!, to_csv
-
 include("orderbooks.jl")
-export Order, Book, add!, update!, to_csv
-
 include("database.jl")
-export Recorder, push!, write, build, teardown
-
 include("process.jl")
-export process
+export process, Message, Order, Book
 
 end # module
