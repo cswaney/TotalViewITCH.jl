6dc3deeb6f3ada33b8529698bb1151431850132f
Colin Swaney <colinswaney@princeton.edu>
2021-07-10 11:58:09 -0400

    Update process.jl

--- test/process.jl
+++ test/process.jl
@@ -5,9 +5,43 @@ using TotalViewITCH
 using Dates
 
 file = "./data/bin/S031413-v41.txt"
-version = 4.1
+version = "4.1"
 date = Date("2013-03-14")
 nlevels = 3
 tickers = ["ALT"]
 dir = "./data/csv"
-process(file, version, date, nlevels, tickers, dir)
+
+open(file, "r") do io
+    clock = 0
+    for _ in 1:10
+        message_size = TotalViewITCH.get_message_size(io)
+        @info "message_size=$message_size"
+        message_type = TotalViewITCH.get_message_type(io)
+        @info "message_type=$message_type"
+        message = TotalViewITCH.get_message(
+            io,
+            message_size,
+            message_type,
+            date,
+            clock,
+            version
+        )
+        @info "message=$message"
+    end
+end
+
+
+
+message_size = TotalViewITCH.get_message_size(io)
+message_type = TotalViewITCH.get_message_type(io)
+payload = TotalViewITCH.unpack(io, "I") # [0x000054f6]::Vector{UInt32}
+Int(payload[1]) # 21750
+
+message_size = TotalViewITCH.get_message_size(io)
+message_type = TotalViewITCH.get_message_type(io)
+payload = TotalViewITCH.unpack(io, "Is") # [0x16ff7604, "O"]::Vector{Any}
+Int(payload[1]) # 385840644
+
+message_size = TotalViewITCH.get_message_size(io)
+message_type = TotalViewITCH.get_message_type(io)
+read(io, message_size - 1)
