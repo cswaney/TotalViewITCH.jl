581a5a97bb832a019c3fc981ad1bffe4432409f1
Colin Swaney <colinswaney@princeton.edu>
2023-03-06 14:30:56 -0500

    Reformat runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,32 @@ using TotalViewITCH
 using Test
 using Dates
 
-# include("messages.jl")
-include("orderbooks.jl")
-# include("database.jl")
+@testset verbose = true "TotalViewITCH.jl" begin
+
+    @testset "messages" begin
+        include("messages.jl")
+    end
+
+    @testset "orderbooks" begin
+        # include("orderbooks.jl")
+    end
+
+    @testset "database" begin
+        # include("database.jl")
+    end
+
+    @testset "process" begin
+        # include("process.jl")
+    end
+
+    # @static if VERSION == v"1.6"
+    #     using Documenter
+    #     @testset "Docs" begin
+    #         DocMeta.setdocmeta!(Flux, :DocTestSetup, :(using Flux); recursive=true)
+    #         doctest(Flux)
+    #     end
+    # end
+
+end
+
+
