c2db15809498117622afe0c5e59dec165cfcb967
Colin Swaney <colinswaney@princeton.edu>
2023-08-19 16:01:28 -0400

    Disable MongoDB testing for now

--- test/backends.jl
+++ test/backends.jl
@@ -28,22 +28,22 @@ end
 
 
 # NOTE: this test requires mongodb running at mongodb://localhost:27017
-@testset "Backend.MongDB" begin
+# @testset "Backend.MongDB" begin
 
-    date = Date("2017-02-27")
-    backend = TotalViewITCH.MongoDB("mongodb://localhost:27017"; db_name="test")
+#     date = Date("2017-02-27")
+#     backend = TotalViewITCH.MongoDB("mongodb://localhost:27017"; db_name="test")
 
-    @test build(backend; force=true)
-    @test ping(backend)["status"] == "ok"
-    @test check_exists(backend)
-    @test teardown(backend; force=true)
-    @test !check_exists(backend)
+#     @test build(backend; force=true)
+#     @test ping(backend)["status"] == "ok"
+#     @test check_exists(backend)
+#     @test teardown(backend; force=true)
+#     @test !check_exists(backend)
 
-    build(backend; force=true)
-    @test insert(backend, [
-            AddMessage(date, 0, 0, 123456789, "A", 'B', 125, 100),
-            AddMessage(date, 0, 0, 123456789, "A", 'B', 125, 100)
-        ],"messages", "A", date) == 2
-    @test clean(date, "A", backend)
+#     build(backend; force=true)
+#     @test insert(backend, [
+#             AddMessage(date, 0, 0, 123456789, "A", 'B', 125, 100),
+#             AddMessage(date, 0, 0, 123456789, "A", 'B', 125, 100)
+#         ],"messages", "A", date) == 2
+#     @test clean(date, "A", backend)
 
-end
+# end
--- test/buffer.jl
+++ test/buffer.jl
@@ -34,30 +34,30 @@ end
 
 
 # NOTE: this test requires mongodb running at mongodb://localhost:27017
-@testset "Buffer.MongoDB" begin
+# @testset "Buffer.MongoDB" begin
 
-    date = Date("2017-02-27")
-    backend = MongoDB("mongodb://localhost:27017"; db_name="test")
-    build(backend; force=true)
+#     date = Date("2017-02-27")
+#     backend = MongoDB("mongodb://localhost:27017"; db_name="test")
+#     build(backend; force=true)
 
-    buffer = Buffer{MongoDB,OrderMessage}(["A"], backend, "messages", date, 2)
+#     buffer = Buffer{MongoDB,OrderMessage}(["A"], backend, "messages", date, 2)
 
-    @test_throws KeyError write(buffer, AddMessage(date, 0, 0, 123456789, "AAPL", 'B', 125, 100)) # missing ticker
+#     @test_throws KeyError write(buffer, AddMessage(date, 0, 0, 123456789, "AAPL", 'B', 125, 100)) # missing ticker
 
-    write(buffer, AddMessage(date, 0, 0, 123456789, "A", 'B', 125, 100))
-    @test buffer.ptrs["A"] == 2 # buffer inserts
+#     write(buffer, AddMessage(date, 0, 0, 123456789, "A", 'B', 125, 100))
+#     @test buffer.ptrs["A"] == 2 # buffer inserts
 
-    write(buffer, AddMessage(date, 0, 0, 123456789, "A", 'S', 127, 100))
-    @test buffer.cnt == 2 # buffer flushes all items
-    @test buffer.ptrs["A"] == 1 # buffer resets
+#     write(buffer, AddMessage(date, 0, 0, 123456789, "A", 'S', 127, 100))
+#     @test buffer.cnt == 2 # buffer flushes all items
+#     @test buffer.ptrs["A"] == 1 # buffer resets
 
-    write(buffer, AddMessage(date, 0, 0, 123456789, "A", 'B', 124, 100))
-    flush(buffer, "A")
-    @test buffer.cnt == 3 # buffer flushes 1 item
-    @test buffer.ptrs["A"] == 1 # buffer resets
+#     write(buffer, AddMessage(date, 0, 0, 123456789, "A", 'B', 124, 100))
+#     flush(buffer, "A")
+#     @test buffer.cnt == 3 # buffer flushes 1 item
+#     @test buffer.ptrs["A"] == 1 # buffer resets
 
-    write(buffer, AddMessage(date, 0, 0, 123456789, "A", 'S', 128, 100))
-    reset(buffer, "A")
-    @test buffer.ptrs["A"] == 1 # buffer resets
+#     write(buffer, AddMessage(date, 0, 0, 123456789, "A", 'S', 128, 100))
+#     reset(buffer, "A")
+#     @test buffer.ptrs["A"] == 1 # buffer resets
 
-end
+# end
