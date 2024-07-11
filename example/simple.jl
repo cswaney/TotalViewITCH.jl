6399d3e027e48b7d25495a7c3903fa43c731082a
Colin Swaney <colinswaney@princeton.edu>
2024-07-11 11:02:05 +0200

    Update simple.jl

--- example/simple.jl
+++ example/simple.jl
@@ -3,7 +3,7 @@ A simple example demonstrating how to parse and read data for a single ticker.
 
 To run with MongoDB backend, first start a local MongoDB server:
 ```shell
-docker run -p 27017:27017 ./data/db:/data/db mongo:latest
+docker run -p 27017:27017 -v ./data/db:/data/db mongo:latest
 ```
 """
 
@@ -20,4 +20,4 @@ backend = MongoDB("mongodb://localhost:27017", "test")
 parser = Parser(backend)
 parser("./data/bin/S031413-v41.txt", Date("2013-03-14"), ["A", "B"], 4.1)
 res = find(backend, "messages", "A", Date("2013-03-14"))
-map(bson -> filter!(((k,v),) -> k !== "_id", Dict(bson)), res)
+dat = Dict.(res)
